43834728478a2417e4d9c7229c64f0d9fef3a2b1
eunseo-kim <67737432+eunseo-kim@users.noreply.github.com>
2021-07-19 22:18:09 +0900

    기능구현 (background, greetings, clock)

--- js/greetings.js
+++ js/greetings.js
@@ -3,6 +3,9 @@ const loginInput = loginForm.querySelector("input");
 const greeting = document.querySelector("#greeting");
 const renameBtn = document.querySelector("#renameBtn");
 const savedUserName = localStorage.getItem("username");
+// 시간별 인사말
+const date = new Date();
+const currentHour = date.getHours();
 
 // localStorage username 검사
 if (savedUserName === null) {
@@ -23,15 +26,26 @@ function logInSubmit(event) {
 
 // greeting
 function greetings(username) {
-  greeting.innerText = `Hello, ${username}`;
+  // 시간대 별 인사
+  if (currentHour >= 6 && currentHour < 12) {
+    greeting.innerText = `Good morning, ${username}`;
+  } else if (currentHour >= 12 && currentHour < 17) {
+    greeting.innerText = `Good afternoon, ${username}`;
+  } else if (currentHour >= 17 && currentHour < 20) {
+    greeting.innerText = `Good evening, ${username}`;
+  } else {
+    greeting.innerText = `Good night, ${username}`;
+  }
   greeting.classList.remove("hidden");
   renameBtn.classList.remove("hidden");
   renameBtn.addEventListener("submit", modifyUsername);
 }
 
-// modify username (from localStorage) - 미완성
-function modifyUsername() {
-  localStorage.removeItem("username");
-  // preventDefault()를 없애서 브라우저 새로고침 되도록 함
-  // 전부 새로고침하지 않고 이름만 바꿀 수는 없을까?
+// modify username (from localStorage) : 전부 새로고침 안하고 이름만 고치기
+function modifyUsername(event) {
+  event.preventDefault();
+  greeting.classList.add("hidden");
+  renameBtn.classList.add("hidden");
+  loginForm.classList.remove("hidden");
+  loginForm.addEventListener("submit", logInSubmit);
 }
